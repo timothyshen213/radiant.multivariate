597b442d5ee8f85d22c1f646c6d5e63d9737b5dd
timothyshen213 <timothyshen213@gmail.com>
2022-08-22 21:12:30 -0700

    pca

--- R/pca.R
+++ R/pca.R
@@ -4,7 +4,6 @@
 #'
 #' @param dataset Dataset
 #' @param vars Vector of variables to include in the analysis
-#' @param labels A vector of labels for the leaves of the tree
 #' @param pca_scale Boolean value if the dataset is to be scaled
 #' @param pca_center Boolean value if the dataset is to be centered
 #' @param pca_pc Maximum principal components (default is equal to the number of variables)
@@ -18,8 +17,9 @@
 #' @import dplyr
 #'
 #' @export
-pca <- function(dataset, vars, labels, pca_scale, pca_center, pca_pc, data_filter = "",
+pca <- function(dataset, vars, pca_scale, pca_center, pca_pc, data_filter = "",
                 envir = parent.frame()){
+  labels == "none"
   df_name <- if (is_string(dataset)) dataset else deparse(substitute(dataset))
   dataset <- get_data(dataset, if (labels == "none") vars else c(labels, vars), filt = data_filter, envir = envir) %>%
     as.data.frame() %>%
